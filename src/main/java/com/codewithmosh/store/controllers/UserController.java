f258f5668b0cf1476331dae607fc23247a409cb5
IPSY-Induwara <ilabs-ishenuka@ipsy.com>
2026-04-28 17:13:52 +0530

    feat : add builtin and custom validations

--- src/main/java/com/codewithmosh/store/controllers/UserController.java
+++ src/main/java/com/codewithmosh/store/controllers/UserController.java
@@ -7,14 +7,18 @@ import com.codewithmosh.store.dtos.UserDto;
 import com.codewithmosh.store.entities.User;
 import com.codewithmosh.store.mappers.UserMapper;
 import com.codewithmosh.store.repositories.UserRepository;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 @RestController
@@ -55,7 +59,7 @@ public class UserController {
 
     @PostMapping
     public ResponseEntity<UserDto>  createUser(
-            @RequestBody RegisterUserRequest request,
+            @Valid  @RequestBody RegisterUserRequest request,
             UriComponentsBuilder uriBuilder
     ){
         var user = userMapper.toEntity(request);
