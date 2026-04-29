b84353bb367ea48428b1273e8e1d88b6fceba4e2
IPSY-Induwara <ilabs-ishenuka@ipsy.com>
2026-04-29 12:43:41 +0530

    feat : add builtin and custom validations

--- src/main/java/com/codewithmosh/store/controllers/UserController.java
+++ src/main/java/com/codewithmosh/store/controllers/UserController.java
@@ -58,10 +58,14 @@ public class UserController {
     }
 
     @PostMapping
-    public ResponseEntity<UserDto>  createUser(
+    public ResponseEntity<?>  registerUser(
             @Valid  @RequestBody RegisterUserRequest request,
             UriComponentsBuilder uriBuilder
     ){
+        if(userRepository.existsByEmail(request.getEmail())){
+            return ResponseEntity.badRequest().body(Map.of("email", "Email is already registered"));
+        }
+
         var user = userMapper.toEntity(request);
         userRepository.save(user);
 
--- src/main/java/com/codewithmosh/store/repositories/UserRepository.java
+++ src/main/java/com/codewithmosh/store/repositories/UserRepository.java
@@ -1,8 +1,12 @@
 package com.codewithmosh.store.repositories;
 
 import com.codewithmosh.store.entities.User;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    boolean existsByEmail(@NotBlank(message = "Email is required") @Email(message = "Email must be valid") String email);
 }
